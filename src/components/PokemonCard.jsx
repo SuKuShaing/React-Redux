cdeb6e3b609c7e5d731d74da695db20b611bd0a7
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-05-14 13:08:14 -0400

    mejoras en los estilos de la imagen

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,12 +2,23 @@ import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 
-const PokemonCard = ({name, number}) => {
+const PokemonCard = ({ name, number }) => {
 	return (
 		<Card
 			title={name}
 			cover={
-				<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`} loading="lazy" height={170} />
+				<img
+					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`}
+					loading="lazy"
+					alt={`imagen del pokemon ${name}`}
+					style={{
+						width: "100%",
+						height: "auto",
+						objectFit: "contain",
+						// maxHeight: "200px",
+						margin: "auto",
+					}}
+				/>
 			}
 			alt={`imagen del pokemon ${name}`}
 			extra={<StarOutlined />}
